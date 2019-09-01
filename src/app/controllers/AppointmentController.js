1b857e30af59abff641ada90e361c7c7bf4d5a14
dantxal <dantxal@gmail.com>
2019-09-01 15:35:33 -0300

    Finished jobs on bee-queue.

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -5,8 +5,8 @@ import Appointment from '../models/Appointment';
 import User from '../models/User';
 import File from '../models/File';
 import Notification from '../schemas/Notification';
-
-import Mail from '../../lib/mail';
+import CancellationMail from '../jobs/CancellationMail';
+import Queue from '../../lib/Queue';
 
 class AppointmentController {
   async store(req, res) {
@@ -147,23 +147,7 @@ class AppointmentController {
     appointment.canceled_at = new Date();
     await appointment.save();
 
-    /**
-     * Send mail to provider
-     */
-
-    await Mail.sendMail({
-      to: `${appointment.provider.name} <${appointment.provider.email}>`,
-      subject: 'Agendamento cancelado',
-      template: 'cancelation',
-      context: {
-        provider: appointment.provider.name,
-        user: appointment.user.name,
-        date: format(appointment.date, "'dia' dd 'de' MMMM', às' H:mm'h'", {
-          locale: pt,
-        }),
-      },
-    });
-
+    await Queue.add(CancellationMail.key, { appointment });
     return res.json(appointment);
   }
 }
